d7cc6c7a68a27af3809d67f1190fa48f939d3c5d
nnnajwa <elouennassnajwa@gmail.com>
2022-05-10 09:20:24 +0200

    commit

---
+++ Services.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+#include "Services.h"
+
+Services::Services ()
+{
+
+} 
+
+
+Services::~Services ( )
+{
+
+}
---
+++ Services.h
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+class Services
+{
+
+public:
+    
+    Services ();
+    virtual ~Services ( );
+
+protected:
+
+int userID;
+string identifiant;
+string mdp;
+};
+
