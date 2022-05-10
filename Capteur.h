bbb6f191b09abf240c438999a56221a806e6a295
Hugo BLAESS <hugo.blaess@insa-lyon.fr>
2022-05-10 12:04:48 +0200

    atmo

--- Capteur.h
+++ Capteur.h
@@ -1,5 +1,8 @@
 #include <string>
+#include <unordered_map>
+#include <vector>
 #include "Position.h"
+#include "Mesure.h"
 using namespace std;
 
 class Capteur
@@ -8,10 +11,13 @@ public:
     
     Capteur (string ID, Position pos);
     virtual ~Capteur ( );
-    double getATMO(string date);
+    int getATMO(string date);
+    int maxi(int, int, int, int);
+    int index(double valeur, double palier1, double palier2, double palier3, double palier4, double palier5, double palier6, double palier7, double palier8, double palier9);
 
 protected:
 
 string sensorID;
 Position position;
+unordered_map<string, vector<Mesure>> mesures;
 };
--- makefile
+++ makefile
@@ -8,14 +8,20 @@ MAP: main
 GETSET: EXTFLAGS += -DGETSET
 GETSET: main
 
-main : Utilisateur.o Position.o main.o 
-		g++ -o main Utilisateur.o Position.o main.o $(EXTFLAGS)
+main : Utilisateur.o Position.o Attribut.o Mesure.o main.o 
+		g++ -o main Utilisateur.o Position.o Attribut.o Mesure.o main.o $(EXTFLAGS)
 
 Utilisateur.o : Utilisateur.cpp
 		g++ -c -g $(EXTFLAGS) Utilisateur.cpp
 
 Position.o : Position.cpp
 		g++ -c -g $(EXTFLAGS) Position.cpp
+
+Attribut.o : Attribut.cpp
+		g++ -c -g $(EXTFLAGS) Attribut.cpp
+
+Mesure.o : Mesure.cpp
+		g++ -c -g $(EXTFLAGS) Mesure.cpp
 
 main.o : main.cpp
 		g++ -c -g $(EXTFLAGS) main.cpp
