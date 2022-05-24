315c9213a9435f7bb0597a41a9bf98b1ce283275
ODuvivier <95276187+ODuvivier@users.noreply.github.com>
2022-05-24 09:02:07 +0200

    Update Services.cpp

--- Services.cpp
+++ Services.cpp
@@ -1,5 +1,6 @@
 using namespace std;
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -39,4 +40,29 @@ double Services::moyenneQualiteAir(Position p, double rayon, string moment)
 vector<Capteur> Services::identifierCapteursNonFiables()
 {
     
+}
+
+vector<Capteur> Services::initCapteur(istream& str )
+{
+    
+    vector<Capteur> capteurs;
+    string ligne;
+    string sensorID;
+    string lat;
+    string lng;
+    
+    while( getline(str,ligne) )
+    {
+        istringstream iss{ligne};
+        getline(iss,sensorID,';');
+        getline(iss,lat,';');
+        getline(iss,lng,';');
+        int latitude = stoi(lat);
+        int longitude = stoi(lng);
+        Capteur capteur(sensorID,latitude,longitude);
+        capteurs.push_back(capteur);
+
+        cout<< sensorID << lat << lng <<endl;
+    }
+    return capteurs;
 }
